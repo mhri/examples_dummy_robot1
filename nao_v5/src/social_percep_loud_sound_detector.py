f57ae7d60e1ef0ba762835fd49382fe538d9c576
Byeong-Kyu Ahn <byeongkyu@gmail.com>
2017-05-19 17:05:03 +0900

    Add social_perception, sensory_perception
    - loud_sound_detection
    - touch_sensor

---
+++ nao_v5/src/social_percep_loud_sound_detector.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#-*- encoding: utf8 -*-
+
+import rospy
+from perception_core.perception_base import PerceptionBase
+from geometry_msgs.msg import PointStamped
+
+class LoudSoundDetector(PerceptionBase):
+    def __init__(self):
+        super(LoudSoundDetector, self).__init__("touch_sensor")
+
+        rospy.Subscriber('loud_sound_detect', PointStamped, self.handle_loud_sound_detected)
+        rospy.loginfo('[%s] initialze done...'%rospy.get_name())
+        rospy.spin()
+
+    def handle_loud_sound_detected(self, msg):
+        write_data = self.conf_data['loud_sound_detection']['data']
+        write_data['xyz'] = [msg.point.x, msg.point.y, msg.point.z]
+        write_data['frame_id'] = msg.header.frame_id
+
+        if msg.point.x >= 0:
+            if msg.point.y > 0.2:
+                write_data['direction'] = 'front_left'
+            elif msg.point.y < -0.2:
+                write_data['direction'] = 'front_right'
+            else:
+                write_data['direction'] = 'front'
+        else:
+            if msg.point.y > 0.2:
+                write_data['direction'] = 'rear_left'
+            elif msg.point.y < -0.2:
+                write_data['direction'] = 'rear_right'
+            else:
+                write_data['direction'] = 'rear'
+
+        self.save_to_memory(self.conf_data.keys()[0], data=write_data)
+        self.raise_event(self.conf_data.keys()[0], 'loud_sound_detected')
+
+if __name__ == '__main__':
+    m = LoudSoundDetector()
---
+++ nao_v5/src/social_percep_touch_sensor.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+#-*- encoding: utf8 -*-
+
+import rospy
+from perception_core.perception_base import PerceptionBase
+from naoqi_bridge_msgs.msg import Bumper, HeadTouch
+from std_msgs.msg import Bool
+
+class TouchSensor(PerceptionBase):
+    def __init__(self):
+        super(TouchSensor, self).__init__("touch_sensor")
+
+        rospy.Subscriber('foot_contact', Bool, self.handle_robot_lifted)
+        rospy.Subscriber('bumper', Bumper, self.handle_bumper_event)
+        rospy.Subscriber('tactile_touch', HeadTouch, self.handle_head_touch_event)
+
+        rospy.loginfo('[%s] initialze done...'%rospy.get_name())
+        rospy.spin()
+
+    def handle_robot_lifted(self, msg):
+        if not msg.data:
+            write_data = self.conf_data['touch_sensor']['data']
+            write_data['touched_part'] = 'robot_lifted'
+
+            self.save_to_memory(self.conf_data.keys()[0], data=write_data)
+            self.raise_event(self.conf_data.keys()[0], 'robot_lifted')
+
+    def handle_bumper_event(self, msg):
+        if msg.state != 1:
+            return
+
+        event_name = '_bumper'
+        if msg.bumper == 0:
+            event_name = 'right' + event_name
+        else:
+            event_name = 'left' + event_name
+
+        write_data = self.conf_data['touch_sensor']['data']
+        write_data['touched_part'] = event_name
+
+        self.save_to_memory(self.conf_data.keys()[0], data=write_data)
+        self.raise_event(self.conf_data.keys()[0], event_name + '_touched')
+
+    def handle_head_touch_event(self, msg):
+        if msg.state != 1:
+            return
+
+        event_name = '_head'
+        if msg.button == 1:
+            event_name = 'front' + event_name
+        elif msg.button == 2:
+            event_name = 'middle' + event_name
+        elif msg.button == 3:
+            event_name = 'rear' + event_name
+        else:
+            return
+
+        write_data = self.conf_data['touch_sensor']['data']
+        write_data['touched_part'] = event_name
+
+        self.save_to_memory(self.conf_data.keys()[0], data=write_data)
+        self.raise_event(self.conf_data.keys()[0], event_name + '_touched')
+
+if __name__ == '__main__':
+    m = TouchSensor()
