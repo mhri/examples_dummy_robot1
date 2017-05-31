882c3492c5cae9eb963097a36d87fd03742c81bc
Byeong-Kyu Ahn <byeongkyu@gmail.com>
2017-05-31 15:05:51 +0900

    Add function for detecting hand touch

--- nao_v5/src/social_percep_touch_sensor.py
+++ nao_v5/src/social_percep_touch_sensor.py
@@ -3,7 +3,7 @@
 
 import rospy
 from perception_core.perception_base import PerceptionBase
-from naoqi_bridge_msgs.msg import Bumper, HeadTouch
+from naoqi_bridge_msgs.msg import Bumper, HeadTouch, HandTouch
 from std_msgs.msg import Bool
 
 class TouchSensor(PerceptionBase):
@@ -13,6 +13,7 @@ class TouchSensor(PerceptionBase):
         rospy.Subscriber('foot_contact', Bool, self.handle_robot_lifted)
         rospy.Subscriber('bumper', Bumper, self.handle_bumper_event)
         rospy.Subscriber('tactile_touch', HeadTouch, self.handle_head_touch_event)
+        rospy.Subscriber('hand_touch', HandTouch, self.handle_hand_touch_event)
 
         rospy.loginfo('[%s] initialze done...'%rospy.get_name())
         rospy.spin()
@@ -60,6 +61,23 @@ class TouchSensor(PerceptionBase):
 
         self.save_to_memory(self.conf_data.keys()[0], data=write_data)
         self.raise_event(self.conf_data.keys()[0], event_name + '_touched')
+
+    def handle_hand_touch_event(self, msg):
+        if msg.state != 1:
+            return
+
+        event_name = '_hand'
+        if msg.hand == 3 or msg.hand == 4 or msg.hand == 5:
+            event_name = 'left' + event_name
+        elif msg.hand == 0 or msg.hand == 1 or msg.hand == 2:
+            event_name = 'right' + event_name
+
+        write_data = self.conf_data['touch_sensor']['data']
+        write_data['touched_part'] = event_name
+
+        self.save_to_memory(self.conf_data.keys()[0], data=write_data)
+        self.raise_event(self.conf_data.keys()[0], event_name + '_touched')
+        
 
 if __name__ == '__main__':
     m = TouchSensor()
